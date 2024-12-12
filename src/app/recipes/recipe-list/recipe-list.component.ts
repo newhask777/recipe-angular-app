2b8ef8b835aad09075bbd77a1b3b19c966898989
newhask777 <newhaskellisp@gmail.com>
2024-12-12 17:27:35 +0300

    finish componnets html

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RecipeItemComponent } from "./recipe-item/recipe-item.component";
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,5 +9,16 @@ import { RecipeItemComponent } from "./recipe-item/recipe-item.component";
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  @Input() recipes: Recipe[] = [
+    new Recipe(
+      "A first test Recipe",
+      "A first Recipe description",
+      "https://www.allrecipes.com/thmb/oWrKte1auLsqVEcnxa8z1vCZdDU=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/67700_RichPastaforthePoorKitchen_ddmfs_4x3_2284-220302ec8328442096df370dede357d7.jpg"
+    ),
+    new Recipe(
+      "A second test Recipe",
+      "A second Recipe description",
+      "https://hips.hearstapps.com/hmg-prod/images/prime-rib-recipe-1633100250-6570ab8245291.jpeg?crop=0.561xw:0.842xh;0.303xw,0.140xh&resize=1200:*"    )
+  ];
 
 }
--- src/app/shoping-list/shoping-list.component.ts
+++ src/app/shoping-list/shoping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { Ingredient } from '../shared/Ingredient.model';
 
 @Component({
   selector: 'app-shoping-list',
@@ -8,5 +9,15 @@ import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
   styleUrl: './shoping-list.component.css'
 })
 export class ShopingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient(
+      "A first test Inhredient",
+      2
+    ),
+    new Ingredient(
+      "A second test Inhredient",
+      5
+    ),
+  ];
 
 }
