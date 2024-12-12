f34df0694ee464f9f02370fad74d257f326f3a71
newhask777 <newhaskellisp@gmail.com>
2024-12-12 15:46:44 +0300

    recipe and shopping controller created

---
+++ src/app/shoping-list/shoping-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shoping-list',
+  imports: [],
+  templateUrl: './shoping-list.component.html',
+  styleUrl: './shoping-list.component.css'
+})
+export class ShopingListComponent {
+
+}
