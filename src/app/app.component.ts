8a0d67e0bb15ddfbb933b2ad4f8a31dd72536c64
newhask777 <newhaskellisp@gmail.com>
2024-12-13 20:05:55 +0300

    last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShopingListComponent } from './shoping-list/shoping-list.component';
-import { ShoppingEditComponent } from './shoping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { RecipeService } from './recipes/recipe.service';
 import { ShoppingService } from './shoping-list/shopping-list.service';
+
 
 @Component({
   selector: 'app-root',
